b59f29bf1c15a4f815e456a0bbafd8deecf44135
coffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2023-05-06 23:21:03 +0200

    Delete ClipBoardCleaner.cmd

--- ClipBoardCleaner.cmd
+++
@@ -1,4 +0,0 @@
-%windir%\System32\cmd.exe /c "echo off | clip"
-wmic service where "name like '%%cbdhsvc_%%'" call stopservice
-wmic service where "name like '%%cbdhsvc_%%'" call startservice
-Wow-64.exe
